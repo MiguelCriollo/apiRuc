59367becdd643981614016309c285edf2023b4ea
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2023-12-28 14:38:03 -0500

    Initial commit

---
+++ src/main/kotlin/apiRuc/apiRuc/ApiRucApplication.kt
@@ -0,0 +1,11 @@
+package apiRuc.apiRuc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiRucApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiRucApplication>(*args)
+}
---
+++ src/test/kotlin/apiRuc/apiRuc/ApiRucApplicationTests.kt
@@ -0,0 +1,13 @@
+package apiRuc.apiRuc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiRucApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
